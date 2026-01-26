5ea8cf83e11b6b2a758c5e1723ef573e9bcfd518
arur17 <2004shivang@gmail.com>
2026-01-26 12:09:33 +0530

    started working on the cat-rs

---
+++ cat-rs/target/debug/cat-rs.d
@@ -0,0 +1,1 @@
+/home/arur17/work/projects/cli-tools/cat-rs/target/debug/cat-rs: /home/arur17/work/projects/cli-tools/cat-rs/src/main.rs
---
+++ cat-rs/target/debug/deps/cat_rs-af9c635f95894de9.d
@@ -0,0 +1,5 @@
+/home/arur17/work/projects/cli-tools/cat-rs/target/debug/deps/cat_rs-af9c635f95894de9.d: src/main.rs
+
+/home/arur17/work/projects/cli-tools/cat-rs/target/debug/deps/cat_rs-af9c635f95894de9: src/main.rs
+
+src/main.rs:
